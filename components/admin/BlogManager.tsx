3b4ba90f7a08473635accd57188ed6a4785e70cd
harshitpathak1238 <harshitpathak1238@gmail.com>
2026-09-05 21:39:46 +0530

    fixed blog page

--- components/admin/BlogManager.tsx
+++ components/admin/BlogManager.tsx
@@ -4,7 +4,7 @@ import { EditorContent, useEditor } from '@tiptap/react';
 import StarterKit from '@tiptap/starter-kit';
 import Image from '@tiptap/extension-image';
 import Link from '@tiptap/extension-link';
-import Table from '@tiptap/extension-table';
+import { Table } from '@tiptap/extension-table';
 import TableRow from '@tiptap/extension-table-row';
 import TableCell from '@tiptap/extension-table-cell';
 import TableHeader from '@tiptap/extension-table-header';
